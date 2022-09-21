45e1f8c31295ac975d4f111b4ecdc2e082323f19
Harsh Naik <harsh@Harshs-MacBook-Pro.local>
2022-09-20 19:11:16 -0700

    This is a new commit for what I originally planned to be amended

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h5>Hello, This is my React Application deployed on Amazon EC2 for the CMPE 281 Lab 1</h5>
+        <h6> Harsh Naik || ID: 016648060</h6>
+      
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
